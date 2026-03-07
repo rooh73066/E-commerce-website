32b5221c69dbaa889d9fed9069159f609c8ca721
rooh73066 <rooh73066@gmail.com>
2026-03-07 17:41:38 +0500

    add out top seller section in the code

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+
+// const scrollAmount = 300; // how much to scroll per click
+// const leftBtn = document.querySelector('.scroll-btn.left');
+// const rightBtn = document.querySelector('.scroll-btn.right');
+// const productCard = document.querySelector('.product-card');
+
+// leftBtn.addEventListener('click', () => {
+//     productCard.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+// });
+
+// rightBtn.addEventListener('click', () => {
+//     productCard.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+// });
